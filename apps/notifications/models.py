e8af72697e107a5ae0064a3e2c14638d94ea83ba
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-03-31 23:30:29 +0800

    fix notification model attribut not shows up

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.models import BaseModel
 
+
 class ScheduleType(models.TextChoices):
     DAILY = "DA", "Daily"
     WEEKLY = "WE", "Weekly"
@@ -11,16 +12,12 @@ class ScheduleType(models.TextChoices):
 class Notifications(BaseModel):
     recepiant = models.CharField(max_length=13)
     message = models.TextField(blank=True)
-    schedule_type = (
-        models.CharField(
-            max_length=2,
-            choices=ScheduleType.choices,
-            default=ScheduleType.DAILY,
-        ),
+    schedule_type = models.CharField(
+        max_length=2,
+        choices=ScheduleType.choices,
+        default=ScheduleType.DAILY,
     )
-    time = models.TimeField(null=True, blank=True)  
-    day_of_week = models.IntegerField(
-        null=True, blank=True
-    )  
-    day_of_month = models.IntegerField(null=True, blank=True)  
-    exact_datetime = models.DateTimeField(null=True, blank=True)  
+    time = models.TimeField(null=True, blank=True)
+    day_of_week = models.IntegerField(null=True, blank=True)
+    day_of_month = models.IntegerField(null=True, blank=True)
+    exact_datetime = models.DateTimeField(null=True, blank=True)
