e2f0163319f2d72f9a5bcaa9cddad7de163c643f
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-04-01 09:00:36 +0800

    edit & delete notification

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views.notification import NotificationView
+from .views.notification import CreateNotificationView, EditNotificationView, delete_notification_view
 from .views.test_send import TestSend
 from .views.auth import LoginView
 from .views.dashboard import DashboardView
@@ -9,5 +9,7 @@ urlpatterns = [
     path("", TestSend.as_view(), name="test_send"),
     path("login/", LoginView.as_view(), name="login"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
-    path("dashboard/notification/create", NotificationView.as_view(), name="notification"),
+    path("dashboard/notification/create", CreateNotificationView.as_view(), name="create-notification"),
+    path("dashboard/notification/<str:id>/edit", EditNotificationView.as_view(), name="edit-notification"),
+    path("dashboard/notification/<str:id>/delete", delete_notification_view, name="delete-notification"),
 ]
--- app/views/notification.py
+++ app/views/notification.py
@@ -1,18 +1,51 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
 from django.views import View
+from app.views import dashboard
 from apps.notifications.forms import NotificationsForm
+from apps.notifications.models import Notifications
 from core.views import LoginRequiredMixinView
 
 
-class NotificationView(LoginRequiredMixinView,View):
-    def get(self,request):
+class CreateNotificationView(LoginRequiredMixinView, View):
+    def get(self, request):
         form = NotificationsForm()
-        return render(request,"notification/index.html", { "form": form})
+        return render(request, "notification/index.html", {"form": form})
 
-    def post(self,request):
+    def post(self, request):
         form = NotificationsForm(request.POST)
-        if form.is_valid():
-            notification = form.save(commit=False)  
-            notification.actor = request.user  
-            notification.save()  
+        if not form.is_valid():
+            return render(request, "notification/index.html", {"form": form})
+
+        notification = form.save(commit=False)
+        notification.actor = request.user
+        notification.save()
+        return redirect(to="dashboard")
+
+
+class EditNotificationView(LoginRequiredMixinView, View):
+    def get(self, request, id):
+        notification = get_object_or_404(Notifications, id=id)
+        if not request.user == notification.actor:
             return redirect(to="dashboard")
+        form = NotificationsForm(instance=notification)
+        return render(request, "notification/edit.html", {"form": form})
+
+    def post(self, request, id):
+        notification = get_object_or_404(Notifications, id=id)
+        if not request.user == notification.actor:
+            return redirect(to="dashboard")
+        form = NotificationsForm(request.POST, instance=notification)
+        if not form.is_valid():
+            return render(request, "notification/edit.html", {"form": form})
+        notification = form.save()
+        return redirect(to="dashboard")
+
+
+@login_required()
+def delete_notification_view(request, id):
+    notification = get_object_or_404(Notifications, id=id)
+    if not request.user == notification.actor:
+        return redirect(to="dashboard")
+    notification.delete()
+    return redirect(to="dashboard")
