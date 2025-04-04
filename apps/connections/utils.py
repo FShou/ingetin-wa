f7a47823c28bc4018bd7c58866ebcfdfecfa08a8
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-04-04 13:31:07 +0800

    restart the waha if not ready

--- apps/connections/utils.py
+++ apps/connections/utils.py
@@ -7,6 +7,8 @@ def check_status():
     print(response)
     if response.status_code == 200:
         status = response.json().get("status") # STOPPED,STARTING, SCAN_QR, CONNECTED
+        if status == "FAILED" or status == "STOPPED":
+            requests.post(f"{WAHA_BASE_URL}/api/sessions/default/restart")
         return status
     return "NOT_READY" 
 
